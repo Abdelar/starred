27fdaf07390feef960f014fea8152a77b2a2c92c
Abdellatif Elaroussi <elaroussi@outlook.com>
2019-10-31 20:40:38 +0100

    testing Bootstrap and font awesome, and adding a template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,73 @@
 <template>
-  <div></div>
+  <div class="container mt-3">
+    <div class="jumbotron jumbotron-fluid rounded">
+      <div class="container">
+        <h1 class="display-4">
+          The Most Starred Repos
+          <font-awesome-icon icon="star" class="text-warning" />
+        </h1>
+        <p class="lead">Submitted in the last 30 days to GitHub.</p>
+        <hr class="my-2" />
+        <p>by Abdellatif ELAROUSSI</p>
+        <p class="lead">
+          <a
+            class="btn btn-primary btn"
+            href="https://developer.github.com/v3/"
+            role="button"
+          >REST API v3</a>
+        </p>
+      </div>
+    </div>
+    <div id="items-group">
+      <div class="row d-flex mb-3 align-items-start">
+        <div class="col-sm-6 col-lg-3">
+          <img
+            class="img-fluid d-block img-thumbnail"
+            src="https://images.unsplash.com/photo-1570819782440-f7d16ec89cfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+            alt="alt text"
+          />
+        </div>
+        <div class="col">
+          <small class="float-sm-right text-muted text-small">
+            <font-awesome-icon icon="calendar-check" class="mr-2" />Submitted
+            <span>date of creation</span> days ago by
+            <span class="text-primary">name of the owner</span>
+          </small>
+          <h3 class="text-primary">name</h3>
+          <p class="mb-0 mb-2 lead">description</p>
+          <span class="badge badge-primary mr-2 p-1">
+            Stars:
+            <span>stars count</span>
+          </span>
+          <span class="badge badge-primary p-1">
+            Issues:
+            <span>issues count</span>
+          </span>
+        </div>
+      </div>
+      <hr />
+    </div>
+  </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      repos: []
+    };
+  },
+  components: { FontAwesomeIcon }
 };
 </script>
 
 <style lang="scss">
+// overriding bootstrap default styles
+$primary: #283943;
+@import "node_modules/bootstrap/scss/bootstrap";
+img {
+  width: 200px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCalendarCheck, faStar } from "@fortawesome/free-solid-svg-icons";
+library.add(faCalendarCheck, faStar);
 
 Vue.config.productionTip = false;
 
