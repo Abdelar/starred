31e7275d4645065083272984504566318f318a69
Abdellatif Elaroussi <elaroussi@outlook.com>
2019-11-05 18:08:21 +0100

    catching error when occurs

--- src/App.vue
+++ src/App.vue
@@ -18,44 +18,51 @@
         </p>
       </div>
     </div>
-    <div v-for="item in repos.items" :key="item.id">
-      <div class="row d-flex mb-3 align-items-start">
-        <div class="col-sm-6 col-lg-3">
-          <img
-            class="img-fluid d-block img-thumbnail"
-            :src="item.owner.avatar_url"
-            :alt="item.owner.login"
-          />
-          <!-- embed a link in the image element to the repo owner -->
-          <!-- <a :href="item.owner.html_url" class="stretched-link" :title="item.owner.login"></a> -->
-        </div>
-        <div class="col">
-          <small class="float-sm-right text-muted text-small">
-            <font-awesome-icon icon="calendar-check" class="mr-2" />Submitted
-            <span>{{formattedDate(item.created_at)}}</span> days ago by
-            <span class="text-primary">
-              <a
-                :href="item.owner.html_url"
-                :title="item.owner.login"
-                class="text-decoration-none"
-              >{{item.owner.login}}</a>
-            </span>
-          </small>
-          <h3 class="text-primary">
-            <a :href="item.html_url" :title="item.name" class="text-decoration-none">{{item.name}}</a>
-          </h3>
-          <p class="mb-0 mb-2 lead">{{item.description}}</p>
-          <span class="badge badge-primary mr-2 p-1">
-            Stars:
-            <span>{{item.stargazers_count}}</span>
-          </span>
-          <span class="badge badge-primary p-1">
-            Issues:
-            <span>{{item.open_issues_count}}</span>
-          </span>
-        </div>
+    <div>
+      <!-- giving a feedback when an error occurred -->
+      <div class="alert alert-warning" v-if="error">
+        {{error}}
       </div>
-      <hr />
+      <!-- listing different repos -->
+      <div v-for="item in repos" :key="item.id">
+        <div class="row d-flex mb-3 align-items-start">
+          <div class="col-sm-6 col-lg-3">
+            <img
+              class="img-fluid d-block img-thumbnail"
+              :src="item.owner.avatar_url"
+              :alt="item.owner.login"
+            />
+            <!-- embed a link in the image element to the repo owner -->
+            <!-- <a :href="item.owner.html_url" class="stretched-link" :title="item.owner.login"></a> -->
+          </div>
+          <div class="col">
+            <small class="float-sm-right text-muted text-small">
+              <font-awesome-icon icon="calendar-check" class="mr-2" />Submitted
+              <span>{{formattedDate(item.created_at)}}</span> days ago by
+              <span class="text-primary">
+                <a
+                  :href="item.owner.html_url"
+                  :title="item.owner.login"
+                  class="text-decoration-none"
+                >{{item.owner.login}}</a>
+              </span>
+            </small>
+            <h3 class="text-primary">
+              <a :href="item.html_url" :title="item.name" class="text-decoration-none">{{item.name}}</a>
+            </h3>
+            <p class="mb-0 mb-2 lead">{{item.description}}</p>
+            <span class="badge badge-primary mr-2 p-1">
+              Stars:
+              <span>{{item.stargazers_count}}</span>
+            </span>
+            <span class="badge badge-primary p-1">
+              Issues:
+              <span>{{item.open_issues_count}}</span>
+            </span>
+          </div>
+        </div>
+        <hr />
+      </div>
     </div>
   </div>
 </template>
@@ -68,7 +75,8 @@ export default {
   name: "app",
   data() {
     return {
-      repos: []
+      repos: [],
+      error: ""
     };
   },
   methods: {
@@ -81,11 +89,12 @@ export default {
       .get(
         "https://api.github.com/search/repositories?q=created:>" +
           moment()
-            .subtract("days", 30)
+            .subtract(30, "days")
             .format("YYYY-MM-DD") +
           "&sort=stars&order=desc"
       )
-      .then(response => (this.repos = response.data));
+      .then(response => (this.repos = response.data.items))
+      .catch(error => (this.error = `Something went wrong: ${error.message}!`));
     //this gets the results of the last 30 days despite what the current date is.
   },
   components: { FontAwesomeIcon }
